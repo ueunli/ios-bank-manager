a48ed665194a0120ea3fe66042b51915b6e0555a
ueunli <cdenen20@gmail.com>
2023-03-05 12:38:08 +0900

    refactor: `ServiceAsynchronizer` 구조체 생성
    - 비동기 작업 생성 및 실행에 관한 로직을 별도의 구조체에 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -8,15 +8,14 @@
 import Foundation
 
 struct Bank {
-    private let queue: Queue<String>
     private let clerks: [BankClerkProtocol]
+    private let customers: Queue<String>
     private let numberOfCustomers: Int
     private let rangeOfNumberOfCustomers = (minimum: 10, maximum: 30)
     private var timer: Timer
-    private let semaphore = DispatchSemaphore(value: 1)
     
     init(clerks: BankingService...) {
-        self.queue = Queue<String>()
+        self.customers = Queue<String>()
         self.clerks = Array(clerksPerType: clerks)
         self.numberOfCustomers = Int.random(in: rangeOfNumberOfCustomers.minimum...rangeOfNumberOfCustomers.maximum)
         self.timer = Timer()
@@ -32,44 +31,14 @@ struct Bank {
     private func lineUpCustomersInQueue() {
         (1...numberOfCustomers).forEach {
             let customer = Customer("\($0)번 고객")
-            queue.enqueue(customer)
+            customers.enqueue(customer)
         }
     }
 
     private func handleAllCustomers() {
-        let works = makeWorkGroup()
+        let serviceManager = ServiceAsynchronizer(queue: customers)
+        let works = serviceManager.makeWorkGroup(by: clerks)
         works.wait()
-    }
-    
-    private func makeWorkGroup() -> DispatchGroup {
-        let group = DispatchGroup()
-        
-        let workItems = clerks.map(makeWorkItem)
-        workItems.forEach {
-            DispatchQueue.global().async(group: group, execute: $0)
-        }
-
-        return group
-    }
-    
-    private func makeWorkItem(by clerk: BankClerkProtocol) -> DispatchWorkItem {
-        let workItem = DispatchWorkItem {
-            while !queue.isEmpty() {
-                guard (queue.peekFirst() as? Customer)?.purposeOfVisit == clerk.service else { continue }
-                
-                semaphore.wait()
-                guard let customer = extractCustomerFromQueue() as? Customer else { semaphore.signal(); return }
-                semaphore.signal()
-                
-                clerk.serve(customer)
-            }
-        }
-        return workItem
-    }
-    
-    private func extractCustomerFromQueue() -> Node<String>? {
-        let customer = queue.dequeue()
-        return customer
     }
     
     mutating func close() {
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/ServiceAsynchronizer.swift
@@ -0,0 +1,48 @@
+//
+//  ServiceAsynchronizer.swift
+//  BankManagerConsoleApp
+//
+//  Created by J.E on 2023/03/05.
+//
+
+import Foundation
+
+struct ServiceAsynchronizer {
+    private let queue: Queue<String>
+    private var group = DispatchGroup()
+    private let thread = DispatchQueue.global()
+    private let semaphore = DispatchSemaphore(value: 1)
+    
+    func makeWorkGroup(by clerks: [BankClerkProtocol]) -> DispatchGroup {
+        let workItems = clerks.map(makeWorkItem)
+        workItems.forEach {
+            thread.async(group: group, execute: $0)
+        }
+
+        return group
+    }
+    
+    private func makeWorkItem(by clerk: BankClerkProtocol) -> DispatchWorkItem {
+        let workItem = DispatchWorkItem {
+            while !queue.isEmpty() {
+                guard (queue.peekFirst() as? Customer)?.purposeOfVisit == clerk.service else { continue }
+                
+                semaphore.wait()
+                guard let customer = extractCustomer(from: queue) else { semaphore.signal(); return }
+                semaphore.signal()
+                
+                clerk.serve(customer)
+            }
+        }
+        return workItem
+    }
+    
+    private func extractCustomer(from queue: Queue<String>) -> Customer? {
+        let customer = queue.dequeue() as? Customer
+        return customer
+    }
+    
+    init(queue: Queue<String>) {
+        self.queue = queue
+    }
+}
