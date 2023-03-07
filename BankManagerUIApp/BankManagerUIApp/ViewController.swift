abd60436bc10f5aaab349baa39d9501d781e3321
longlivedrgn <kimye9808@naver.com>
2023-03-07 10:32:27 +0900

    feat: UI요소들 생성 및 추가

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -7,12 +7,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var timerLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "업무시간"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private var waitingLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "대기중"
+        label.textAlignment = .center
+        label.backgroundColor = .green
+        
+        return label
+    }()
+    
+    private var workingLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "업무중"
+        label.textAlignment = .center
+        label.backgroundColor = .blue
+        
+        return label
+    }()
+    
+    private var addTenCustomersInQueueButton: UIButton = {
+        let button = UIButton()
+        
+        button.titleLabel?.text = "고객 10명 추가"
+        button.addTarget(ViewController.self, action: #selector(addTenCustomersInQueueButtonTapped), for: .touchUpInside)
+        button.titleLabel?.textColor = .blue
+        return button
+    }()
+    
+    private var resetCustomersInQueueButton: UIButton = {
+        let button = UIButton()
+        
+        button.titleLabel?.text = "초기화"
+        button.addTarget(ViewController.self, action: #selector(resetCustomersInQueueButtonTapped), for: .touchUpInside)
+        button.titleLabel?.textColor = .red
+        return button
+    }()
+    
+    private var buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 0
+        stackView.axis = .horizontal
+        
+        return stackView
+    }()
+    
+    private var statusLabelStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 0
+        stackView.axis = .horizontal
+        
+        return stackView
+    }()
+    
+    private var headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 0
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc private func addTenCustomersInQueueButtonTapped() {
+        
+    }
+    
+    @objc private func resetCustomersInQueueButtonTapped() {
+        
+    }
 }
 
